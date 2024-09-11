13fc033c63c669bbab12439125d57308d698e957
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-09-11 18:33:38 -0500

    app structure

---
+++ src/app/components/form-component/form-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-component.component.html',
+  styleUrl: './form-component.component.scss'
+})
+export class FormComponentComponent {
+
+}
