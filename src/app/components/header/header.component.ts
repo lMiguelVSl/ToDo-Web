6a0a455afcd7ac0ceeeb9cbe075dc63581fb36a5
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-09-11 20:35:04 -0500

    Header component and navigation refactored

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../shared/material/material.module';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, RouterOutlet, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
