31cf2a5902b6311a7f88e2058ddc368b9dc511bf
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-09-11 22:28:40 -0500

    home component

--- src/app/views/home-page/home.component.ts
+++ src/app/views/home-page/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-component',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
